95e76fe6bb83e42c9bf0a1e2414682a681841bbf
Tamires <tamires.lowande@gmail.com>
2019-10-14 20:48:14 +0000

    group queries

---
+++ 3_queries_group-by/avr_comp_time.sql
@@ -0,0 +1,7 @@
+
+  SELECT students.name AS student, AVG(duration) AS average_assignment_duration
+  FROM assignment_submissions 
+  JOIN students ON students.id = student_id 
+  WHERE students.end_date IS NULL
+  GROUP BY student
+  ORDER BY average_assignment_duration DESC;
